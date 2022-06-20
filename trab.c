287ab070d4dfe9d4212546dfd2849d4b260b6070
Joao-Schio <joao.ortega.linux@gmail.com>
2022-06-20 17:14:56 -0400

    Terminei ?

--- trab.c
+++ trab.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-
+/* Aluno: João Pedro Schio Ortega - RGA 2021.1905.022-5
+   discente: Francisco Eloi Soares de Araujo */
 
 
 
@@ -23,6 +24,40 @@ typedef struct lista{
 
 
 
+void troca (REG *a, REG*b){
+    REG aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+
+void selection (cel *lst, int *n){
+    cel *i, *j, *min;
+    for (i = lst; i->prox != NULL; i = i->prox){
+        min = i;
+        for (j = i->prox; j != NULL; j = j->prox){
+            if (j->reg.codigo < min->reg.codigo){
+                min = j;
+            }
+        }
+        troca(&i->reg, &min->reg);
+        *n = *n + 1;
+    }
+}
+
+
+void insertion (cel *lst, int *n){
+    cel *i, *j;
+    for (i = lst; i->prox != NULL; i = i->prox){
+        for (j = i->prox; j != NULL; j = j->prox){
+            if (i -> reg.validade > j -> reg.validade){
+                troca(&i->reg, &j->reg);
+                *n = *n + 1;
+            }
+        }
+    }
+}
+
 
 
 
@@ -35,18 +70,25 @@ int main(void){
     p -> prox = NULL;
     REG aux;
     FILE *entrada = fopen("produtos.ent","r");
+    int TrocasS = 0;
+    int TrocasI = 0;
     while(fscanf(entrada,"%d %d",&aux.codigo,&aux.validade) != EOF){
-        p -> reg = aux;
         p -> prox = (cel*)malloc(sizeof(cel));
         p -> prox -> ant = p;
         p = p -> prox;
+        p -> reg = aux;
+        p -> prox = NULL;
     }
 
-    for(p = lista; p -> prox != NULL; p = p -> prox){
-        printf("%d %d\n",p -> reg.codigo,p -> reg.validade);
+    selection(lista, &TrocasS);
+    insertion(lista, &TrocasI);
+
+    for (p = lista -> prox ; p != NULL; p = p->prox){
+        printf("%d %d\n",p->reg.codigo,p->reg.validade);
     }
+    printf("Trocas S = %d Trocas I = %d\n",TrocasS,TrocasI);
 
 
-
+    return 0;
 
 }
